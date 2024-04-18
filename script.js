fe45ff30169520a7c2b4fc0cc1596f19862201ec
ThibautCadiou <cadioucontact@gmail.com>
2024-04-18 09:58:21 +0200

    creation de fonctions et de docstring

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+/**
+ * Récupérer la liste des travaux et des catégories depuis l'API grace aux routes indiquées
+ * @param {string} worksPath Le chemin ou l'url de l'api donnant la liste des travaux
+ * @param {string} catPath Le chemin ou l'url de l'API donnant la lits edes catégories
+ * @returns {Array} une liste dont le premier élément est la liste des travaux et le second la liste des catégories
+ */
+async function recupererTravauxEtCategories(worksPath, catPath) {
+    // Récupération des travaux
+    const reponseWorks = await fetch(worksPath);
+    const travaux = await reponseWorks.json();
+
+    // Récupération des catégories
+    const reponseCategories = await fetch(catPath);
+    const categories = await reponseCategories.json();
+
+    return [travaux, categories];
+}
+
+/**
+ * Permet d'afficher la liste de travaux envoyé en paramètre
+ * @param {Array} myList La liste des travaux à afficher
+ */
+function afficherListe(myList) {
+    const baliseParentFigure = document.querySelector(".gallery");
+    for (let i = 0; i < myList.length; i++) {
+        const element = myList[i];
+
+        const figureElement = document.createElement("figure");
+
+        const imageElement = document.createElement("img");
+        imageElement.src = element.imageUrl;
+
+        const figureCaptionElement = document.createElement("figcaption");
+        figureCaptionElement.innerText = element.title;
+
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figureCaptionElement);
+        baliseParentFigure.appendChild(figureElement);
+    }
+}
+
 // Récupération des bouttons
 let baliseBoutons = document.querySelectorAll(".filtres button");
 for (let i = 0; i < baliseBoutons.length; i++) {
@@ -7,33 +48,7 @@ for (let i = 0; i < baliseBoutons.length; i++) {
     });
 }
 
-// Création de la liste des pièces
-// let baliseBoutonTous = document.querySelector(' button[name="tous"]');
-// baliseBoutonTous.addEventListener("click", async function (event) {
-// });
-const reponseWorks = await fetch("http://localhost:5678/api/works");
-const travaux = await reponseWorks.json();
-console.log(travaux);
-
-// Récupération des catégories
-const reponseCategories = await fetch("http://localhost:5678/api/categories");
-const categories = await reponseCategories.json();
-// console.log(categories);
-
-// Création des fiches et affichage
-const baliseParentFigure = document.querySelector(".gallery");
-for (let i = 0; i < travaux.length; i++) {
-    const element = travaux[i];
-
-    const figureElement = document.createElement("figure");
-
-    const imageElement = document.createElement("img");
-    imageElement.src = element.imageUrl;
-
-    const figureCaptionElement = document.createElement("figcaption");
-    figureCaptionElement.innerText = element.title;
-
-    figureElement.appendChild(imageElement);
-    figureElement.appendChild(figureCaptionElement);
-    baliseParentFigure.appendChild(figureElement);
-}
+let travauxEtCategories = await recupererTravauxEtCategories("http://localhost:5678/api/works", "http://localhost:5678/api/categories");
+let travaux = travauxEtCategories[0];
+let categories = travauxEtCategories[1];
+afficherListe(travaux);
