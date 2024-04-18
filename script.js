473cf0adaecc0e9db5834cf8680f96dfc70ca5ac
ThibautCadiou <cadioucontact@gmail.com>
2024-04-18 09:25:24 +0200

    Correction du css de base

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!");
-
 // Récupération des bouttons
 let baliseBoutons = document.querySelectorAll(".filtres button");
 for (let i = 0; i < baliseBoutons.length; i++) {
@@ -10,9 +8,32 @@ for (let i = 0; i < baliseBoutons.length; i++) {
 }
 
 // Création de la liste des pièces
-let baliseBoutonTous = document.querySelector(' button[name="tous"]');
-baliseBoutonTous.addEventListener("click", async function (event) {
-    const reponse = await fetch("http://localhost:5678/api/works");
-    const tous = await reponse.json();
-    console.log(tous);
-});
+// let baliseBoutonTous = document.querySelector(' button[name="tous"]');
+// baliseBoutonTous.addEventListener("click", async function (event) {
+// });
+const reponseWorks = await fetch("http://localhost:5678/api/works");
+const travaux = await reponseWorks.json();
+console.log(travaux);
+
+// Récupération des catégories
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponseCategories.json();
+// console.log(categories);
+
+// Création des fiches et affichage
+const baliseParentFigure = document.querySelector(".gallery");
+for (let i = 0; i < travaux.length; i++) {
+    const element = travaux[i];
+
+    const figureElement = document.createElement("figure");
+
+    const imageElement = document.createElement("img");
+    imageElement.src = element.imageUrl;
+
+    const figureCaptionElement = document.createElement("figcaption");
+    figureCaptionElement.innerText = element.title;
+
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(figureCaptionElement);
+    baliseParentFigure.appendChild(figureElement);
+}
