c6bae379f5147658a9264312c2c667ec5dbc2076
ThibautCadiou <cadioucontact@gmail.com>
2024-04-18 15:16:11 +0200

    changement pour les boutons: génération en js au lieu de création en html + css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ******** Fonctions Fonctionnelles ********
+
 /**
  * Récupérer la liste des travaux et des catégories depuis l'API grace aux routes indiquées
  * @param {string} worksPath Le chemin ou l'url de l'api donnant la liste des travaux
@@ -20,7 +22,7 @@ async function recupererTravauxEtCategories(worksPath, catPath) {
  * Permet d'afficher la liste de travaux envoyé en paramètre
  * @param {Array} myList La liste des travaux à afficher
  */
-function afficherListe(myList) {
+function afficherTravaux(myList) {
     const baliseParentFigure = document.querySelector(".gallery");
     for (let i = 0; i < myList.length; i++) {
         const element = myList[i];
@@ -39,31 +41,59 @@ function afficherListe(myList) {
     }
 }
 
-function filtrerTravaux(travauxFiltres, categories, filtreApplique, verbose = 0) {
-    console.clear();
-    // verbose !== 0
-    //     ? console.log(`
-    // travauxFiltres : ${typeof travauxFiltres}
-    // categories     : ${categories}
-    // filtreApplique : ${filtreApplique}
-    // `)
-    //     : null;
+/**
+ * On vide la gallery pour l'affichage des travaux filtré
+ */
+function viderGallery() {
+    let baliseGallery = document.querySelector(".gallery");
+    baliseGallery.innerHTML = "";
+}
+
+// ******** Fonctions en cours de dev ********
+function filtrerTravaux(travaux, categories, filtreApplique, verbose = 0) {
+    let travauxfiltres = [];
     switch (filtreApplique) {
         case "tous":
+            travauxfiltres = [...travaux];
             verbose === 0 ? null : console.log('on applique le filtre "Tous"');
             break;
         case "objets":
+            travauxfiltres = [travaux[0]];
             verbose === 0 ? null : console.log('on applique le filtre "Objets"');
             break;
         case "appartements":
+            travauxfiltres = [travaux[1]];
             verbose === 0 ? null : console.log('on applique le filtre "Appartements"');
             break;
         case "hotel-et-restaurants":
+            travauxfiltres = [travaux[2]];
             verbose === 0 ? null : console.log('on applique le filtre "Hotels & restaurants"');
             break;
         default:
             console.log("Le filtre demandé n'existe pas mec ...");
             break;
+    }
+    return travauxfiltres;
+}
+
+/**
+ *
+ * @param {Array} categories Liste des catégories (sans TOUS) avec leurs identifiants et leurs nom
+ */
+function genererBouttons(categories) {
+    console.log("Hello world!");
+    let categoriesAvecTous = [{ id: 0, name: "Tous" }];
+    categoriesAvecTous.push(...categories);
+    let balisePortfolio = document.querySelector(".filtres"); // élément parent pour l'ajout des boutons
+    for (let i = 0; i < categoriesAvecTous.length; i++) {
+        const element = categoriesAvecTous[i];
+
+        let baliseElementEnfant = document.createElement("button");
+        baliseElementEnfant.classList.add("filtres-rapides");
+        baliseElementEnfant.classList.add("filtres-inactif");
+        baliseElementEnfant.id = element.id;
+        baliseElementEnfant.innerText = element.name;
+        balisePortfolio.appendChild(baliseElementEnfant);
     }
 }
 
@@ -73,16 +103,21 @@ let travauxEtCategories = await recupererTravauxEtCategories("http://localhost:5
 let travaux = travauxEtCategories[0];
 let categories = travauxEtCategories[1];
 
-// On fait ce que l'on peut poto !!!
-let filtreApplique = "tous"; // paramètre qui servira à filtrer les travaux
-let baliseBoutons = document.querySelectorAll(".filtres button"); // Récupération des bouttons
-for (let i = 0; i < baliseBoutons.length; i++) {
-    const button = baliseBoutons[i];
-    button.addEventListener("click", (event) => {
-        filtreApplique = event.target.name;
-        let travauxFiltres = filtrerTravaux(travaux, categories, filtreApplique, 1); // On applique le filtre sur les traveaux en fonction de la catégorie
-    });
-}
+// console.log(categories);
+// console.log(travaux);
 
-//on affiche les travaux filtrés
-// afficherListe(travauxFiltres);
+genererBouttons(categories);
+// On fait ce que l'on peut poto !!!
+// let filtreApplique = "tous"; // paramètre qui servira à filtrer les travaux
+// let baliseBoutons = document.querySelectorAll(".filtres button"); // Récupération des bouttons
+// let travauxFiltres = [];
+// for (let i = 0; i < baliseBoutons.length; i++) {
+//     const button = baliseBoutons[i];
+//     button.addEventListener("click", (event) => {
+//         viderGallery(); //on vide la gallery
+//         filtreApplique = event.target.name; // on recupère le nom associé au bouton
+//         travauxFiltres = filtrerTravaux(travaux, categories, filtreApplique, 1); // On applique le filtre sur les traveaux en fonction de la catégorie
+//         //on affiche les travaux filtrés
+//         afficherTravaux(travauxFiltres);
+//     });
+// }
