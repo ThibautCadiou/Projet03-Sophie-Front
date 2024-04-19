daa6764060ff61e31860d4363852283abb8b0b46
ThibautCadiou <cadioucontact@gmail.com>
2024-04-19 14:37:16 +0200

    export total de la partie generation projet dans le fichjier gallerie.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,4 @@
-import { filtrerTravaux, recupererTravauxEtCategories, afficherTravaux, viderGallery, genererBouttons, toggleButton } from "/scripts/gallerie.js";
+import { genererProjets } from "/scripts/gallerie.js";
 
 // ******** Main ********
-// On récupère les travaux et les catégories
-let travauxEtCategories = await recupererTravauxEtCategories("http://localhost:5678/api/works", "http://localhost:5678/api/categories");
-let travaux = travauxEtCategories[0];
-let categories = travauxEtCategories[1];
-
-categories = genererBouttons(categories); // on génère les boutons avec les infos de la route /categories
-
-let baliseBoutons = document.querySelectorAll(".filtres button"); //Récupération des bouttons
-let listeFiltresID = [];
-let travauxFiltres = [...travaux];
-afficherTravaux(travauxFiltres); // on met tous les travaux pour le démarrage
-
-for (let i = 0; i < baliseBoutons.length; i++) {
-    const element = baliseBoutons[i];
-
-    element.addEventListener("click", (event) => {
-        //console.log(event.target.id); // on récupère le texte à l'intérieur
-        listeFiltresID = toggleButton(event.target.id, listeFiltresID);
-        let travauxFiltres = filtrerTravaux(travaux, categories, listeFiltresID, 1);
-        viderGallery();
-        afficherTravaux(travauxFiltres);
-    });
-}
+genererProjets();
