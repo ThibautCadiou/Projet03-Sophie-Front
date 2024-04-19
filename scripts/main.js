bbe0dec0b614ff245b1f176d47470f057babaafe
ThibautCadiou <cadioucontact@gmail.com>
2024-04-19 15:42:47 +0200

    finalisation du css de la page de connexion et bonne interaction lors de l'appui des onglets projet et login !!!

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,26 @@
 import { genererProjets } from "/scripts/gallerie.js";
 
 // ******** Main ********
-genererProjets();
+genererProjets(); // pour générer la page de base
+
+// ******** Login ********
+let boutonLogin = document.querySelector(".login");
+let boutonProjet = document.querySelector(".projets");
+
+let baliseProjet = document.querySelector("#affichage-projets");
+let baliseLogin = document.querySelector("#affichage-login");
+
+boutonLogin.addEventListener("click", () => {
+    boutonLogin.style.fontWeight = "800";
+    boutonProjet.style.fontWeight = "400";
+    baliseLogin.style.display = "block";
+    baliseProjet.style.display = "none";
+});
+
+boutonProjet.addEventListener("click", (event) => {
+    boutonLogin.style.fontWeight = "400";
+    boutonProjet.style.fontWeight = "800";
+
+    baliseLogin.style.display = "none";
+    baliseProjet.style.display = "block";
+});
