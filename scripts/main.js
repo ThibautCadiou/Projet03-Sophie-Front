9af04b794a3899f45ab523e36ee7048fac8b86a9
ThibautCadiou <cadioucontact@gmail.com>
2024-04-26 22:25:30 +0200

    Clean d'une partie du code de modal.js v1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { genererProjets } from "/scripts/gallerie.js";
 import { initLogin, testerConnexion } from "/scripts/login.js";
-import { afficherMiniTravaux, setModalToAddPicture } from "/scripts/modal.js";
+import { afficherMiniTravaux, setModalToAddPicture, ouvertureModal } from "/scripts/modal.js";
 
 // ******** Main ********
 let travaux = await genererProjets(); // pour générer la page de base
@@ -13,69 +13,6 @@ baliseFormulaire.addEventListener("submit", async function (event) {
     testerConnexion(event); // on récupère le token
 });
 
-/*
+// ******** Modal ********
+ouvertureModal();
 afficherMiniTravaux(travaux);
-
-
-// Pour ajouter un travail, notamment la photo
-let baliseAjouterPhoto = document.querySelector(".add-on-click");
-let baliseImgageFichierAAjouter = document.querySelector(".image-to-add");
-let baliseRechercheInfosImage = document.querySelector(".mode-without-src");
-let cheminFichier = "";
-baliseAjouterPhoto.addEventListener("click", () => {
-    let baliseChoisirFichier = document.querySelector("#choisir-fichier");
-
-    baliseChoisirFichier.addEventListener("change", (event) => {
-        let fichiers = [];
-        fichiers = event.target.files;
-        cheminFichier = URL.createObjectURL(fichiers[0]);
-        baliseImgageFichierAAjouter.src = cheminFichier;
-        baliseRechercheInfosImage.style.display = "none";
-    });
-    baliseChoisirFichier.click();
-});
-
-function envoyerNewFormdata(myToken, chargeUtile) {
-    let reponse = fetch("http://localhost:5678/api/works", {
-        method: "POST",
-        headers: {
-            // accept: "application/json",
-            Authorization: `Bearer ${myToken}`,
-        },
-        body: chargeUtile, //JSON.stringify(chargeUtile),
-    });
-    console.log(reponse);
-}
-
-let baliseAjoutPhoto = document.querySelector(".ajout-photo");
-
-baliseAjoutPhoto.addEventListener("click", (event) => {
-    let innerTextValue = baliseAjoutPhoto.textContent;
-
-    if (innerTextValue === "Valider") {
-        //Liste des infos a récupérer pour envoyer le travail
-        let baliseInputPath = document.querySelector("#choisir-fichier");
-        let baliseInputName = document.querySelector("#titre");
-        let baliseInputCategorie = document.querySelector("#categorie");
-        let categorieId = "1"; //correspondanceCategorieEtId(baliseInputCategorie.value);
-        console.log(cheminFichier);
-        // on trouve la ccorepsondance entre la catégorie et l'id de la categorie
-
-        //on valide la présence des différents champs
-
-        //on créer l'object formdata
-        const newWork = new FormData(); //créer un objet
-        newWork.append("image", cheminFichier); //imageUrl
-        console.log(cheminFichier);
-        newWork.append("title", baliseInputName.value);
-        newWork.append("category", categorieId); //categoryId
-        // newWork.append("userId", 1);
-        const myToken = localStorage.getItem("token");
-        // console.log(myToken);
-        envoyerNewFormdata(myToken, newWork);
-    } else {
-        setModalToAddPicture();
-    }
-});
-
-*/
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,8 +1,22 @@
-let baliseMiniTravaux = null;
 /**
- * Fonction qui gère la génération des travaux dans la modale 01
+ * Fonction 200 : gère l'ouverture de la modale
+ */
+export function ouvertureModal() {
+    const baliseModal = document.querySelector(".modal");
+    const baliseOverlay = document.querySelector(".overlay");
+    const baliseOpenButton = document.querySelector(".modifier");
+    // creation de la modale pour l'ajout de photo
+    baliseOpenButton.addEventListener("click", (event) => {
+        baliseOverlay.classList.remove("hidden"); // on affiche le calque du background
+        baliseModal.classList.remove("hidden"); // on affiche la modale
+    });
+}
+
+/**
+ * Fonction  210 :Gère la génération des travaux dans la modale 01
  * @param {Array.<object>} listeTravaux
  */
+let baliseMiniTravaux = null;
 export function afficherMiniTravaux(listeTravaux) {
     for (let i = 0; i < listeTravaux.length; i++) {
         const element = listeTravaux[i];
@@ -27,20 +41,16 @@ export function afficherMiniTravaux(listeTravaux) {
     baliseMiniTravaux = document.querySelectorAll(".fa-trash-can");
     for (let i = 0; i < baliseMiniTravaux.length; i++) {
         const element = baliseMiniTravaux[i];
-        // console.log(element);
     }
 }
-
-let balisemodalAddPhoto = document.querySelector(".modal-add-photo");
-let baliseModalTitle = document.querySelector(".modal-title");
-let baliseGallery = document.querySelector(".modal-gallery");
-let baliseAjoutPhoto = document.querySelector(".ajout-photo");
-let baliseBackArrow = document.querySelector(".back-modal");
-let baliseRechercheInfosImage = document.querySelector(".mode-without-src");
 
 /**
  * Fonction qui gère l'affichage de la modale en mode 2
  */
+let baliseModalTitle = document.querySelector(".modal-title");
+let baliseGallery = document.querySelector(".modal-gallery");
+let baliseBackArrow = document.querySelector(".back-modal");
+let balisemodalAddPhoto = document.querySelector(".modal-add-photo");
 export function setModalToAddPicture() {
     baliseModalTitle.textContent = "Ajout photo";
     baliseGallery.style.display = "none";
@@ -50,15 +60,15 @@ export function setModalToAddPicture() {
 }
 
 /**
- * Fonction qui gère l'affichage de la modela en mode 1
+ * Fonction qui gère l'affichage de la modale en mode 1
  */
+let baliseRechercheInfosImage = document.querySelector(".mode-without-src");
 export function setModalToNormal() {
     baliseModalTitle.textContent = "Gallerie photo";
     baliseGallery.style.display = "grid";
     baliseAjoutPhoto.textContent = "Ajouter une photo";
     baliseBackArrow.style.display = "none";
     balisemodalAddPhoto.style.display = "none";
-    // baliseImgageFichierAAjouter.src = null;
     baliseRechercheInfosImage.style.display = "flex";
 }
 
@@ -66,27 +76,19 @@ baliseBackArrow.addEventListener("click", (event) => {
     setModalToNormal();
 });
 
-/******** Partie Modale ********/
 const baliseModal = document.querySelector(".modal");
-const baliseOverlay = document.querySelector(".overlay");
-const baliseCloseButton = document.querySelector(".close-modal");
-
-const baliseOpenButton = document.querySelector(".modal-button"); //utiliser "modifier" ou "modal-button" pour du debug et mettre son display à true ou decommenter dan sle html ou utiliser "modifier"
-// creation de la modale pour l'ajout de photo
-baliseOpenButton.addEventListener("click", (event) => {
-    baliseOverlay.classList.remove("hidden");
-    baliseModal.classList.remove("hidden");
-});
-
 const closeModal = (event) => {
     baliseOverlay.classList.add("hidden");
     baliseModal.classList.add("hidden");
 };
 
+const baliseCloseButton = document.querySelector(".close-modal");
 baliseCloseButton.addEventListener("click", () => {
     closeModal();
     setModalToNormal();
 });
+
+const baliseOverlay = document.querySelector(".overlay");
 
 baliseOverlay.addEventListener("click", () => {
     closeModal();
@@ -102,5 +104,62 @@ document.addEventListener("keydown", (event) => {
     if (event.key === "Escape" && !baliseModal.classList.contains("hidden")) {
         setModalToNormal();
         closeModal();
+    }
+});
+
+// Pour ajouter un travail, notamment la photo
+let baliseAjouterPhoto = document.querySelector(".add-on-click");
+let baliseImgageFichierAAjouter = document.querySelector(".image-to-add");
+let cheminFichier = "";
+baliseAjouterPhoto.addEventListener("click", () => {
+    let baliseChoisirFichier = document.querySelector("#choisir-fichier");
+
+    baliseChoisirFichier.addEventListener("change", (event) => {
+        let fichiers = [];
+        fichiers = event.target.files;
+        cheminFichier = URL.createObjectURL(fichiers[0]);
+        baliseImgageFichierAAjouter.src = cheminFichier;
+
+        let baliseRechercheInfosImage = document.querySelector(".mode-without-src");
+        baliseRechercheInfosImage.style.display = "none";
+    });
+    baliseChoisirFichier.click();
+});
+
+function envoyerNewFormdata(myToken, chargeUtile) {
+    let reponse = fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+            // accept: "application/json",
+            Authorization: `Bearer ${myToken}`,
+        },
+        body: chargeUtile, //JSON.stringify(chargeUtile),
+    });
+}
+
+let baliseAjoutPhoto = document.querySelector(".ajout-photo");
+baliseAjoutPhoto.addEventListener("click", (event) => {
+    let innerTextValue = baliseAjoutPhoto.textContent;
+
+    if (innerTextValue === "Valider") {
+        //Liste des infos a récupérer pour envoyer le travail
+        let baliseInputPath = document.querySelector("#choisir-fichier");
+        let baliseInputName = document.querySelector("#titre");
+        let baliseInputCategorie = document.querySelector("#categorie");
+        let categorieId = "1"; //correspondanceCategorieEtId(baliseInputCategorie.value);
+        // on trouve la ccorepsondance entre la catégorie et l'id de la categorie
+
+        //on valide la présence des différents champs
+
+        //on créer l'object formdata
+        const newWork = new FormData(); //créer un objet
+        newWork.append("image", cheminFichier); //imageUrl
+        newWork.append("title", baliseInputName.value);
+        newWork.append("category", categorieId); //categoryId
+        // newWork.append("userId", 1);
+        const myToken = localStorage.getItem("token");
+        envoyerNewFormdata(myToken, newWork);
+    } else {
+        setModalToAddPicture();
     }
 });
