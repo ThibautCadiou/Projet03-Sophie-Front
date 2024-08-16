75adba9e42fc6793afb0cedbf04696d0530ba3f2
ThibautCadiou <cadioucontact@gmail.com>
2024-08-16 16:42:31 +0200

    Modification des routes pour le bakcend

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-import { genererProjets } from "/scripts/gallerie.js";
-import { initLogin } from "/scripts/login.js";
-import { creerMiniTravaux } from "/scripts/modal.js";
-import { suppressionTravail } from "/scripts/suppress-work.js";
-import {} from "/scripts/add-work.js";
+import { genererProjets } from '/scripts/gallerie.js';
+import { initLogin } from '/scripts/login.js';
+import { creerMiniTravaux } from '/scripts/modal.js';
+import { suppressionTravail } from '/scripts/suppress-work.js';
+import {} from '/scripts/add-work.js';
 
 // ******** Paths ********
-export const workPath = "http://localhost:5678/api/works";
-export const catPath = "http://localhost:5678/api/categories";
-export const addPath = "http://localhost:5678/api/works";
-export const loginPath = "http://localhost:5678/api/users/login";
+export const workPath = 'https://thibautcadiou.github.io/Projet03-Sophie-Back/api/works';
+export const catPath = 'https://thibautcadiou.github.io/Projet03-Sophie-Back/api/categories';
+export const addPath = 'https://thibautcadiou.github.io/Projet03-Sophie-Back/api/works';
+export const loginPath = 'https://thibautcadiou.github.io/Projet03-Sophie-Back/api/users/login';
 
 // ******** Main ********
 await genererProjets(); // pour générer la page de base
