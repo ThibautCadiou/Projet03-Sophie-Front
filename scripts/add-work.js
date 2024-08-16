2da0cb13a76011c74c5f951600da73e1479b1671
ThibautCadiou <cadioucontact@gmail.com>
2024-08-16 16:52:23 +0200

    Modification des routes pour le bakcend v2

--- scripts/add-work.js
+++ scripts/add-work.js
@@ -1,6 +1,6 @@
-import { setModalToAddPicture, closeModal, setModalToNormal } from "/scripts/modal.js";
-import { addPath } from "/scripts/main.js";
-import { recupererCategories, resetAfichage } from "/scripts/gallerie.js";
+import { setModalToAddPicture, closeModal, setModalToNormal } from '/scripts/modal.js';
+import { addPath } from '/scripts/main.js';
+import { recupererCategories, resetAfichage } from '/scripts/gallerie.js';
 
 /**
  * Fonction qui vérifie que les champs sont bien remplis afin de pouvoir Valider l'envoi
@@ -9,71 +9,71 @@ import { recupererCategories, resetAfichage } from "/scripts/gallerie.js";
  * @param {*} b3 balise 03 : la catégorie de la photo
  */
 function verifierChamps(b1, b2, b3) {
-    // console.log(`can send work avant  : ${canSendWork}`);
+  // console.log(`can send work avant  : ${canSendWork}`);
 
-    if (b1.value === "" || b2.value === "" || b3.value === "none") {
-        baliseAjoutPhoto.classList.add("btnValiderGris");
-        baliseAjoutPhoto.classList.add("desactiver-boutons");
-        canSendWork = false;
-    } else {
-        baliseAjoutPhoto.classList.remove("btnValiderGris");
-        baliseAjoutPhoto.classList.remove("desactiver-boutons");
-        canSendWork = true;
-    }
+  if (b1.value === '' || b2.value === '' || b3.value === 'none') {
+    baliseAjoutPhoto.classList.add('btnValiderGris');
+    baliseAjoutPhoto.classList.add('desactiver-boutons');
+    canSendWork = false;
+  } else {
+    baliseAjoutPhoto.classList.remove('btnValiderGris');
+    baliseAjoutPhoto.classList.remove('desactiver-boutons');
+    canSendWork = true;
+  }
 }
 
 //add event listeners sur  la photo
-export let baliseChoisirFichier = document.querySelector("#choisir-fichier");
-baliseChoisirFichier.addEventListener("change", function (event) {
-    verifierChamps(baliseChoisirFichier, baliseTitreNewWork, baliseParentMenuDreoulant);
+export let baliseChoisirFichier = document.querySelector('#choisir-fichier');
+baliseChoisirFichier.addEventListener('change', function (event) {
+  verifierChamps(baliseChoisirFichier, baliseTitreNewWork, baliseParentMenuDreoulant);
 });
 //add event listeners sur le titre
-const baliseTitreNewWork = document.querySelector("#titre");
-baliseTitreNewWork.addEventListener("change", function (event) {
-    verifierChamps(baliseChoisirFichier, baliseTitreNewWork, baliseParentMenuDreoulant);
+const baliseTitreNewWork = document.querySelector('#titre');
+baliseTitreNewWork.addEventListener('change', function (event) {
+  verifierChamps(baliseChoisirFichier, baliseTitreNewWork, baliseParentMenuDreoulant);
 });
 //add event listeners sur la categorie
-const baliseParentMenuDreoulant = document.querySelector("#categorie"); // on génère le formulaire avec les valeur pour les catégories
-baliseParentMenuDreoulant.addEventListener("change", function (event) {
-    verifierChamps(baliseChoisirFichier, baliseTitreNewWork, baliseParentMenuDreoulant);
+const baliseParentMenuDreoulant = document.querySelector('#categorie'); // on génère le formulaire avec les valeur pour les catégories
+baliseParentMenuDreoulant.addEventListener('change', function (event) {
+  verifierChamps(baliseChoisirFichier, baliseTitreNewWork, baliseParentMenuDreoulant);
 });
 /******** Fin de la section validation des champs ********/
 
 /**
  * Fonctions qui réagi au click sur " + Ajouter photo"
  */
-let baliseAjouterPhoto = document.querySelector(".add-on-click");
+let baliseAjouterPhoto = document.querySelector('.add-on-click');
 let myFile = null;
 let objectURL = null;
-let baliseRechercheInfosImage = document.querySelector(".mode-without-src");
-let baliseImageMinitature = document.querySelector(".photo");
-baliseAjouterPhoto.addEventListener("click", () => {
-    let baliseChoisirFichier = document.querySelector("#choisir-fichier");
+let baliseRechercheInfosImage = document.querySelector('.mode-without-src');
+let baliseImageMinitature = document.querySelector('.photo');
+baliseAjouterPhoto.addEventListener('click', () => {
+  let baliseChoisirFichier = document.querySelector('#choisir-fichier');
 
-    baliseChoisirFichier.removeEventListener("change", changementFichier); // on le supprime sil existe deja
-    myFile = baliseChoisirFichier.addEventListener("change", changementFichier);
-    baliseChoisirFichier.click();
+  baliseChoisirFichier.removeEventListener('change', changementFichier); // on le supprime sil existe deja
+  myFile = baliseChoisirFichier.addEventListener('change', changementFichier);
+  baliseChoisirFichier.click();
 });
 
-const baliseApercuImage = document.querySelector(".image-to-add");
+const baliseApercuImage = document.querySelector('.image-to-add');
 function changementFichier(event) {
-    let fichiers = [];
-    fichiers = event.target.files;
-    myFile = fichiers[0]; // on fait l'hypothèse que seule un fichier est récupérer
-    if (myFile !== undefined || myFile !== null) {
-        objectURL = URL.createObjectURL(myFile); //pour pouvoir récupérer l'url de l'image sélectionnée
-        baliseRechercheInfosImage.style.display = "none";
-        baliseApercuImage.src = objectURL;
-        baliseImageMinitature.style.display = "flex";
-        return myFile;
-    }
+  let fichiers = [];
+  fichiers = event.target.files;
+  myFile = fichiers[0]; // on fait l'hypothèse que seule un fichier est récupérer
+  if (myFile !== undefined || myFile !== null) {
+    objectURL = URL.createObjectURL(myFile); //pour pouvoir récupérer l'url de l'image sélectionnée
+    baliseRechercheInfosImage.style.display = 'none';
+    baliseApercuImage.src = objectURL;
+    baliseImageMinitature.style.display = 'flex';
+    return myFile;
+  }
 }
 
 export function resetImage() {
-    myFile = null;
-    baliseApercuImage.src = null;
-    baliseRechercheInfosImage.style.display = "flex";
-    baliseChoisirFichier.value = null;
+  myFile = null;
+  baliseApercuImage.src = null;
+  baliseRechercheInfosImage.style.display = 'flex';
+  baliseChoisirFichier.value = null;
 }
 /**
  * Fonction qui renvoie l'identifiant associé au nom de la categorie selectionnée dans le menu deroulant
@@ -81,20 +81,20 @@ export function resetImage() {
  * @returns ns le menu deroulant
  */
 async function provideIdFromName(name) {
-    const categorie = await recupererCategories();
-    const myId = categorie.filter(function (categorie) {
-        return categorie.name === name;
-    })[0].id;
-    return myId;
+  const categorie = await recupererCategories();
+  const myId = categorie.filter(function (categorie) {
+    return categorie.name === name;
+  })[0].id;
+  return myId;
 }
 
 //Event listeners pour récupérer la catégories en fonction de la selection sur le menu deroulant
 let categorieId = 0;
-let baliseInputCategorie = document.querySelector("#categorie");
-baliseInputCategorie.addEventListener("change", async function (event) {
-    if (baliseInputCategorie.value !== "none") {
-        categorieId = await provideIdFromName(baliseInputCategorie.value);
-    }
+let baliseInputCategorie = document.querySelector('#categorie');
+baliseInputCategorie.addEventListener('change', async function (event) {
+  if (baliseInputCategorie.value !== 'none') {
+    categorieId = await provideIdFromName(baliseInputCategorie.value);
+  }
 });
 
 /**
@@ -103,55 +103,55 @@ baliseInputCategorie.addEventListener("change", async function (event) {
  * @param {object} chargeUtile L'objet à envoyer
  */
 async function envoyerNewFormdata(myToken, chargeUtile) {
-    try {
-        let reponse = await fetch(addPath, {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${myToken}`,
-                accept: "application/json",
-                // "Content-Type": "multipart/form-data",
-            },
-            body: chargeUtile,
-        });
-        if (reponse.ok) {
-            closeModal();
-            setModalToNormal();
-            resetAfichage();
-        } else {
-            throw new Error("Informations d'envoie de fichier incorrect");
-        }
-    } catch (error) {
-        console.log(`Erreur : ${error}`);
+  try {
+    let reponse = await fetch(addPath, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${myToken}`,
+        accept: 'application/json',
+        // "Content-Type": "multipart/form-data",
+      },
+      body: chargeUtile,
+    });
+    if (reponse.ok) {
+      closeModal();
+      setModalToNormal();
+      resetAfichage();
+    } else {
+      throw new Error("Informations d'envoie de fichier incorrect");
     }
+  } catch (error) {
+    console.log(`Erreur : ${error}`);
+  }
 }
 
-let baliseInputName = document.querySelector("#titre");
-let workTitle = "";
-baliseInputName.addEventListener("change", () => {
-    workTitle = baliseInputName.value;
+let baliseInputName = document.querySelector('#titre');
+let workTitle = '';
+baliseInputName.addEventListener('change', () => {
+  workTitle = baliseInputName.value;
 });
 
 export function setCanSendWork(value) {
-    canSendWork = value;
+  canSendWork = value;
 }
 
 export let canSendWork = false;
-export let baliseAjoutPhoto = document.querySelector(".ajout-photo");
-baliseAjoutPhoto.addEventListener("click", function (event) {
-    if (event.target.classList.contains("ajout-photo")) {
-        if (canSendWork) {
-            //on créer l'object formdata
-            const newWork = new FormData();
-            newWork.append("title", workTitle);
-            newWork.append("image", myFile);
-            newWork.append("category", categorieId);
+export let baliseAjoutPhoto = document.querySelector('.ajout-photo');
+baliseAjoutPhoto.addEventListener('click', function (event) {
+  if (event.target.classList.contains('ajout-photo')) {
+    if (canSendWork) {
+      //on créer l'object formdata
+      const newWork = new FormData();
+      newWork.append('title', workTitle);
+      newWork.append('image', myFile);
+      newWork.append('category', categorieId);
 
-            const myToken = localStorage.getItem("token");
+      const myToken = localStorage.getItem('token');
 
-            envoyerNewFormdata(myToken, newWork);
-            canSendWork = false;
-        } else {
-            setModalToAddPicture();
-        }
+      envoyerNewFormdata(myToken, newWork);
+      canSendWork = false;
+    } else {
+      setModalToAddPicture();
     }
+  }
 });
--- scripts/suppress-work.js
+++ scripts/suppress-work.js
@@ -1,5 +1,5 @@
-import { recupererTravaux, afficherTravaux, viderGallery } from "/scripts/gallerie.js";
-import { creerMiniTravaux, viderMinyGallery } from "/scripts/modal.js";
+import { recupererTravaux, afficherTravaux, viderGallery } from '/scripts/gallerie.js';
+import { creerMiniTravaux, viderMinyGallery } from '/scripts/modal.js';
 
 /**
  * Fonction qui permet de renvoyer l'id d'un travail que l'on souhaite supprimer en fonction de l'url de l'image de ce meme travail
@@ -7,10 +7,10 @@ import { creerMiniTravaux, viderMinyGallery } from "/scripts/modal.js";
  * @returns l'Id du l'objet travaux que l'on souhaite supprimer
  */
 function renvoyerIdFromUrl(url, travaux) {
-    const myImg = travaux.filter(function (travail) {
-        return travail.imageUrl === url;
-    });
-    return myImg[0].id;
+  const myImg = travaux.filter(function (travail) {
+    return travail.imageUrl === url;
+  });
+  return myImg[0].id;
 }
 
 /**
@@ -18,25 +18,25 @@ function renvoyerIdFromUrl(url, travaux) {
  * @param {*} event l'event lié au click sur une trashcan
  */
 export async function supprimerTravail(event) {
-    let travaux = await recupererTravaux();
-    const elementClique = event.target;
+  let travaux = await recupererTravaux();
+  const elementClique = event.target;
 
-    const parent = elementClique.parentElement; // on recupere le parent
-    const childImage = parent.firstChild; // dans ses enfants, on trouve le travail qui a ce chemin 'imageUrl'
-    const imageUrl = childImage.src;
-    const id = renvoyerIdFromUrl(imageUrl, travaux); //on récupère l'id du travail en question
+  const parent = elementClique.parentElement; // on recupere le parent
+  const childImage = parent.firstChild; // dans ses enfants, on trouve le travail qui a ce chemin 'imageUrl'
+  const imageUrl = childImage.src;
+  const id = renvoyerIdFromUrl(imageUrl, travaux); //on récupère l'id du travail en question
 
-    // on fait la requete pour supprimer le travail
-    const fetchPathForDelete = "http://localhost:5678/api/works/" + id;
-    const myToken = localStorage.getItem("token");
-    let response = await fetch(fetchPathForDelete, {
-        method: "DELETE",
-        headers: {
-            accept: "*/*",
-            Authorization: `Bearer ${myToken}`,
-        },
-        body: { id: id },
-    });
+  // on fait la requete pour supprimer le travail
+  const fetchPathForDelete = 'https://thibautcadiou.github.io/Projet03-Sophie-Back/api/works/' + id;
+  const myToken = localStorage.getItem('token');
+  let response = await fetch(fetchPathForDelete, {
+    method: 'DELETE',
+    headers: {
+      accept: '*/*',
+      Authorization: `Bearer ${myToken}`,
+    },
+    body: { id: id },
+  });
 }
 
 /**
@@ -44,54 +44,54 @@ export async function supprimerTravail(event) {
  * @param {*} event l'event lié au click sur une trashcan
  */
 export async function supprimerTravailFromSrc(src) {
-    let travaux = await recupererTravaux();
+  let travaux = await recupererTravaux();
 
-    const imageUrl = src;
-    const id = renvoyerIdFromUrl(imageUrl, travaux); //on récupère l'id du travail en question
+  const imageUrl = src;
+  const id = renvoyerIdFromUrl(imageUrl, travaux); //on récupère l'id du travail en question
 
-    // on fait la requete pour supprimer le travail
-    const fetchPathForDelete = "http://localhost:5678/api/works/" + id;
-    const myToken = localStorage.getItem("token");
-    let response = await fetch(fetchPathForDelete, {
-        method: "DELETE",
-        headers: {
-            accept: "*/*",
-            Authorization: `Bearer ${myToken}`,
-        },
-        body: { id: id },
-    });
+  // on fait la requete pour supprimer le travail
+  const fetchPathForDelete = 'https://thibautcadiou.github.io/Projet03-Sophie-Back/api/works/' + id;
+  const myToken = localStorage.getItem('token');
+  let response = await fetch(fetchPathForDelete, {
+    method: 'DELETE',
+    headers: {
+      accept: '*/*',
+      Authorization: `Bearer ${myToken}`,
+    },
+    body: { id: id },
+  });
 }
 
 export async function cliqueSurCorbeilles() {
-    let balisePouvellesMiniTravaux = document.querySelectorAll(".myCard .fa-trash-can");
-    for (let i = 0; i < balisePouvellesMiniTravaux.length; i++) {
-        const element = balisePouvellesMiniTravaux[i];
-        element.addEventListener("click", async function (event) {
-            await supprimerTravail(event);
-            await majGalleries();
-            balisePouvellesMiniTravaux = document.querySelectorAll(".myCard .fa-trash-can");
-        });
-    }
-    return balisePouvellesMiniTravaux;
+  let balisePouvellesMiniTravaux = document.querySelectorAll('.myCard .fa-trash-can');
+  for (let i = 0; i < balisePouvellesMiniTravaux.length; i++) {
+    const element = balisePouvellesMiniTravaux[i];
+    element.addEventListener('click', async function (event) {
+      await supprimerTravail(event);
+      await majGalleries();
+      balisePouvellesMiniTravaux = document.querySelectorAll('.myCard .fa-trash-can');
+    });
+  }
+  return balisePouvellesMiniTravaux;
 }
 
 export async function majGalleries() {
-    viderGallery();
-    viderMinyGallery();
+  viderGallery();
+  viderMinyGallery();
 
-    let travaux = await recupererTravaux();
-    await afficherTravaux(travaux);
-    await creerMiniTravaux(travaux);
+  let travaux = await recupererTravaux();
+  await afficherTravaux(travaux);
+  await creerMiniTravaux(travaux);
 }
 
 export async function suppressionTravail() {
-    let balisePoubellesParent = document.querySelector(".modal-gallery");
-    balisePoubellesParent.addEventListener("click", async function (event) {
-        if (event.target.classList.contains("fa-trash-can")) {
-            const parentElement = event.target.parentNode;
-            const premierEnfant = parentElement.firstChild;
-            await supprimerTravailFromSrc(premierEnfant.src);
-            majGalleries();
-        }
-    });
+  let balisePoubellesParent = document.querySelector('.modal-gallery');
+  balisePoubellesParent.addEventListener('click', async function (event) {
+    if (event.target.classList.contains('fa-trash-can')) {
+      const parentElement = event.target.parentNode;
+      const premierEnfant = parentElement.firstChild;
+      await supprimerTravailFromSrc(premierEnfant.src);
+      majGalleries();
+    }
+  });
 }
